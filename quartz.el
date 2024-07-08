e6360879a34f8fa1f5e5202902f7b1bd794b9990
Pavel Popov <pavel@findhotel.net>
2024-07-08 09:55:03 +0200

    Add quartz--move-assets and quartz--ensure-directory functions

--- quartz.el
+++ quartz.el
@@ -22,12 +22,25 @@
 (defvar quartz-source-directories '("~/Documents/Notes")
   "List of source directories to find notes to publish.")
 
+(defun quartz--ensure-directory (dir)
+  (unless (file-directory-p dir)
+    (make-directory dir)))
+
+(defun quartz--move-assets ()
+  "Move assets from static directory to content directory."
+  (let ((src-dir (file-name-concat quartz-target-dir "static" "assets/"))
+        (tgt-dir (file-name-concat quartz-target-dir "content" "assets/")))
+    (quartz--ensure-directory tgt-dir)
+    (quartz--ensure-directory src-dir)
+    (dolist (f (directory-files src-dir t directory-files-no-dot-files-regexp))
+      (rename-file f tgt-dir t))
+    (quartz--ensure-directory src-dir)))
+
 (defun quartz-export (dir)
   "Export Org files from DIR into Quartz markdown files."
   (interactive
    (list (completing-read "Directory: " quartz-source-directories)))
   (delete-directory (file-name-concat quartz-target-dir "content") t)
-  ;; (make-directory (file-name-concat quartz-target-dir "static/"))
   (dolist (f (quartz-exported-notes dir))
     (with-current-buffer (find-file-noselect (file-name-concat dir f))
       (let ((org-hugo-base-dir quartz-target-dir)
@@ -39,9 +52,13 @@
             )
         (message "exporting %s to Quartz" f)
         (org-hugo-export-to-md))))
-  (rename-file (file-name-concat quartz-target-dir "static" "assets")
-               (file-name-concat quartz-target-dir "content/"))
+  (quartz--move-assets)
   (message "Quartz markdown files and assets exported."))
+
+(defun quartz-open-target-dir ()
+  "Open quartz-target-dir in dired."
+  (interactive)
+  (dired quartz-target-dir))
 
 (defun quartz-export-this-file ()
   "Export current Org file into Quartz markdown file."
@@ -54,8 +71,8 @@
         ;; (org-export-with-tasks nil)
         )
     (message "exporting %s to Quartz" (buffer-file-name))
-    (org-hugo-export-to-md)))
-
+    (org-hugo-export-to-md))
+  (quartz--move-assets))
 
 (defun quartz-build ()
   "Build HTML files from Markdown files using Quartz."
@@ -70,12 +87,10 @@
   (let ((default-directory quartz-target-dir))
     (async-shell-command "make run" "*quartz-server*")))
 
-
 (comment
  (quartz-exported-notes "~/Documents/Notes")
  (quartz-export "~/Documents/Notes")
  )
-
 
 (provide 'quartz)
 ;;; quartz.el ends here
